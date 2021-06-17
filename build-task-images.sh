47c7d0f949a021e57d3fb24d4a516740d9fca589
Camila Souza <camila.odsouza@gmail.com>
2021-06-17 01:58:55 -0300

    Update build-task-images.sh

--- build-task-images.sh
+++ build-task-images.sh
@@ -8,4 +8,5 @@ fi
 
 VERSION=$1
 docker build -t code-challenge/download-data:$VERSION download_data
+docker build -t code-challenge/clean-dataset:$VERSION clean_dataset
 docker build -t code-challenge/make-dataset:$VERSION make_dataset
